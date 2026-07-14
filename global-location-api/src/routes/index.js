6e6087cb1e28cc7a1c0eb80a808333d5cfda82d8
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-07-15 00:34:25 +0700

    refactor: remove global-location-api service and related files
    - Deleted package.json, app.js, config, controllers, middleware, routes, services, and swagger files.
    - This commit removes the entire global-location-api service, including all its dependencies and configurations.

--- global-location-api/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const geocodeRoutes = require('./geocode.routes');
-
-router.use('/', geocodeRoutes);
-
-module.exports = router;
