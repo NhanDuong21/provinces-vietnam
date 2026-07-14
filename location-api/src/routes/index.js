66ca4daa11b3aad73d363414190d1369e91c8b14
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-07-15 00:26:37 +0700

    refactor: remove location-api module and its related files
    - Deleted package.json, scripts, controllers, middleware, routes, services, utils, and swagger files related to the location-api.
    - This cleanup removes the entire location-api functionality, including geocoding and location management.

--- location-api/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const locationRoutes = require('./location.routes');
-const geocodeRoutes = require('./geocode.routes');
-
-router.use('/', locationRoutes);
-router.use('/', geocodeRoutes);
-
-module.exports = router;
--- location-api/src/routes/location.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const locationController = require('../controllers/location.controller');
-
-router.get('/provinces', locationController.getProvinces);
-router.get('/provinces/:provinceId/wards', locationController.getWards);
-
-module.exports = router;
